83527a7d8cf665d9c18376106cdea7caaea6886f
Slate452 <kenechukwuogbuagu@gmail.com>
2022-11-14 22:52:31 +0100

    for Next competition

--- NewVote/diswebhook.py
+++
@@ -1,4 +0,0 @@
-from discord_webhook import discord_webhook
-
-
-webhook = DiscordWebhook(url ='https://discord.com/api/webhooks/1041809583930343435/xNE3cI-ednAb6WjQubuyZCKQAay1lmG4ibVmqk3zhGM9wqswXUVyBk2U7sLT6YfWmwV3',content = message)
